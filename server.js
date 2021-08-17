5e01f154b010b63b76dfbb4a654866f8012561f7
Brady Bott <bottbrady@gmail.com>
2021-08-17 09:44:30 -0600

    fixing errors

--- server.js
+++ server.js
@@ -3,13 +3,25 @@ const app = express()
 
 const path = require('path')
 
+// include and initialize the rollbar library with your access token
+const Rollbar = require('rollbar')
+const rollbar = new Rollbar({
+  accessToken: '5d9af6741c5b4857a55eaaa2a5369b8b',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
+
 app.use(express.json())
 
 const port = process.env.PORT || 4040
 
 app.use(express.static('public'))
 app.get('/', function(req,res) {
-   res.sendFile(path.join(__dirname), '/public/index.html')
+   res.sendFile(path.join(__dirname, '/public/index.html'))
 }) 
 
 
