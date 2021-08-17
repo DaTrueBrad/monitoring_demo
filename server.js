2600c84b32f36774f9b5c992b35e9d5869385409
Brady Bott <bottbrady@gmail.com>
2021-08-17 09:39:15 -0600

    fixing errors

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.json())
 const port = process.env.PORT || 4040
 
 app.use(express.static('public'))
-app.get('/', (req,res) => res.sendFile(path.join(__dirname), '/public/index.html'))
+app.get('/', (req,res) => res.sendFile(path.join(__dirname), './public/index.html'))
 
 
 app.listen(port, () => console.log(`Running away with port ${port}!!`))
